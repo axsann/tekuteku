33af5e6655140f6561c521da21906e89206ec89a
kanta <santoorino3@yahoo.co.jp>
2015-05-08 14:31:36 +0900

    イメージに合っているかを問う6段階のアンケートもDBに文字列として格納

--- tekuteku.go
+++ tekuteku.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 	"github.com/codegangsta/martini-contrib/render"
 	"github.com/go-martini/martini"
-	//_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	_ "github.com/mattn/go-sqlite3"
+	//_ "github.com/go-sql-driver/mysql"
+	"html/template"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -39,6 +40,11 @@ type User struct {
 	Finished  bool   // 終了したかどうか
 }
 
+type Feeling struct {
+	ID   int
+	Text string `sql:"size:10"`
+}
+
 // オノマトペを格納する
 type Onom struct {
 	Index int    // インデックス番号
@@ -47,32 +53,53 @@ type Onom struct {
 
 // HTMLのフォームに保持するデータ
 type SubData struct {
-	UserID        int      // 被験者ID(ユーザID)
-	CurrentOnom   Onom     // 現在のオノマトペ
-	ShuffledOnoms []string // シャッフルしたオノマトペの配列
+	UserID              int      // 被験者ID(ユーザID)
+	CurrentOnom         Onom     // 現在のオノマトペ
+	ShuffledOnomStrings []string // シャッフルしたオノマトペの配列
+	FeelStrings         []string // 6段階アンケート用の文字列の配列
 }
 
 var db gorm.DB // グローバル変数としてgorm.DBを宣言
 // 実験に用いるオノマトペ
-var onoms = []string{"てくてく", "すたすた", "のろのろ", "とぼとぼ"}
+var onomStrings = []string{"てくてく", "すたすた", "のろのろ", "とぼとぼ"}
+var feelStrings = []string{"全くそう思わない", "そう思わない", "あまりそう思わない", "ややそう思う", "そう思う", "とてもそう思う"}
 
 // DBと接続、DBのMigrate(自動設定)
 func init() {
 	db, _ = gorm.Open("sqlite3", "ExperimentA.db")
 	db.DB()
-	db.AutoMigrate(&User{}, &Experiment{})
+	db.AutoMigrate(&User{}, &Experiment{}, &Feeling{})
+	setupFeelingTable()
 }
 
 func main() {
 	m := martini.Classic()
 	// render html template
-	m.Use(render.Renderer())
+	m.Use(render.Renderer(render.Options{
+		Funcs: []template.FuncMap{
+			{
+				"add": func(a, b int) int { return a + b },
+			},
+		},
+	}))
 	m.Get("/", top)                      // トップページ(被験者データの入力ページ)
 	m.Post("/tutrial", tutrial)          // 動作確認ページ
 	m.Post("/first", first)              // 最初のオノマトペの実験ページ
 	m.Post("/since_second", sinceSecond) // 2番目以降のオノマトペの実験ページ、終了ページ
+	m.Get("/show", show)
 	m.Run()
+}
 
+func setupFeelingTable() {
+	var feeling Feeling
+	if db.First(&feeling).RecordNotFound() { // テーブルが空ならばデータを追加する
+		for i := 0; i < len(feelStrings); i++ {
+			feeling.ID = i + 1
+			feeling.Text = feelStrings[i]
+			db.NewRecord(feeling)
+			db.Create(&feeling)
+		}
+	}
 }
 
 // トップページ(被験者データの入力ページ)
@@ -98,32 +125,33 @@ func tutrial(ren render.Render, req *http.Request) {
 	subData.UserID = me.ID // subDataにユーザIDをセット
 
 	// オノマトペをシャッフルする
-	shuffledOnoms := make([]string, len(onoms))
-	perm := rand.Perm(len(onoms))
+	shuffledOnomStrings := make([]string, len(onomStrings))
+	perm := rand.Perm(len(onomStrings))
 	for i, v := range perm {
-		shuffledOnoms[v] = onoms[i]
+		shuffledOnomStrings[v] = onomStrings[i]
 	}
 
-	subData.ShuffledOnoms = shuffledOnoms // subDataにシャッフルしたオノマトペを格納
+	subData.ShuffledOnomStrings = shuffledOnomStrings // subDataにシャッフルしたオノマトペを格納
 	ren.HTML(200, "tutrial", subData)
 }
 
 func first(ren render.Render, req *http.Request) {
-	req.ParseForm()                               // req.Formの取得のためにパース
-	shuffledOnoms := req.Form["shuffled-onoms[]"] // シャッフルしたオノマトペを取得
+	req.ParseForm()                                            // req.Formの取得のためにパース
+	shuffledOnomStrings := req.Form["shuffled-onom-strings[]"] // シャッフルしたオノマトペを取得
 	var subData SubData
 	subData.UserID, _ = strconv.Atoi(req.FormValue("user-id")) // subDataにユーザIDを格納
-	subData.ShuffledOnoms = shuffledOnoms                      // subDataにシャッフルしたオノマトペを格納
+	subData.ShuffledOnomStrings = shuffledOnomStrings          // subDataにシャッフルしたオノマトペを格納
+	subData.FeelStrings = feelStrings
 	var onom Onom
-	onom.Index = 0               // 最初のオノマトペなので、インデックス番号は0
-	onom.Name = shuffledOnoms[0] // シャッフルしたオノマトペの最初のもの
-	subData.CurrentOnom = onom   // subDataにオノマトペを格納
+	onom.Index = 0                     // 最初のオノマトペなので、インデックス番号は0
+	onom.Name = shuffledOnomStrings[0] // シャッフルしたオノマトペの最初のもの
+	subData.CurrentOnom = onom         // subDataにオノマトペを格納
 	ren.HTML(200, "tekuteku", subData)
 }
 
 func sinceSecond(ren render.Render, req *http.Request) {
-	req.ParseForm()                               // req.Formの取得のためにパース
-	shuffledOnoms := req.Form["shuffled-onoms[]"] // シャッフルしたオノマトペを取得
+	req.ParseForm()                                            // req.Formの取得のためにパース
+	shuffledOnomStrings := req.Form["shuffled-onom-strings[]"] // シャッフルしたオノマトペを取得
 	// 実験データをフォームから取得
 	var experiment Experiment
 	experiment.OnomName = req.FormValue("onom-name")
@@ -144,23 +172,35 @@ func sinceSecond(ren render.Render, req *http.Request) {
 
 	var onom Onom
 	currentIndex := experiment.OnomIndex + 1 // インデックス番号を1進める
-	if currentIndex < len(onoms) {           // オノマトペの数だけ実験ページを表示する
+	if currentIndex < len(onomStrings) {     // オノマトペの数だけ実験ページを表示する
 		// 今回表示するオノマトペを格納
 		onom.Index = currentIndex
-		onom.Name = shuffledOnoms[currentIndex]
+		onom.Name = shuffledOnomStrings[currentIndex]
 
 		var subData SubData
-		subData.CurrentOnom = onom            // subDataにオノマトペを格納
-		subData.UserID = userID               // subDataにユーザIDを格納
-		subData.ShuffledOnoms = shuffledOnoms // subDataにシャッフルしたオノマトペを格納
+		subData.CurrentOnom = onom                        // subDataにオノマトペを格納
+		subData.UserID = userID                           // subDataにユーザIDを格納
+		subData.ShuffledOnomStrings = shuffledOnomStrings // subDataにシャッフルしたオノマトペを格納
+		subData.FeelStrings = feelStrings
 
 		ren.HTML(200, "tekuteku", subData)
 	} else { // すべてのオノマトペが終わったら終了ページを表示する
 		fmt.Println("おわり")
 		var me User
-		db.Where("id = ?", userID).First(&me)
-		me.Finished = true
-		db.Save(&me)
+		db.Where("id = ?", userID).First(&me) // ユーザIDで自分の被験者データを取得
+		me.Finished = true                    // 最後まで実験をしたら、被験者データのFinishedをtrueにする
+		db.Save(&me)                          // 再保存(Update)する
 		ren.HTML(200, "end", nil)
 	}
+}
+
+// テスト用
+func show(ren render.Render) {
+	var me User
+	db.First(&me, 2) // 2番目の被験者を取得
+	fmt.Println(me)
+	var experiments []Experiment
+	db.Model(&me).Related(&experiments) // 2番目の被験者の実験データ(複数)を取得
+	fmt.Println(experiments)
+	ren.JSON(200, experiments) // JSONでレンダリング
 }
